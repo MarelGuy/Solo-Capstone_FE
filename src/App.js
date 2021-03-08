8e297f47c0b3ad02e74df3fd711631f1dfd67de6
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-03-08 13:22:49 +0100

    added base pages (boilerplate)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import './index.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Scpage from "./pages/Scpage"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/scp/:id" exact component={Scpage} />
+      <Route path="/signup" exact component={Signup} />
+    </Router>
   );
 }
 
