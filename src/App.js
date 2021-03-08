1059a21695b2592b6f9668b7e22c965879f74c21
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-03-08 15:11:24 +0100

    added container

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Signup from "./pages/Signup"
 import ForumPage from "./pages/ForumPage"
 import NewForum from "./pages/NewForum"
 import NewScp from "./pages/NewScp"
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Home} />
-      {/* Post pages */}
-      <Route path="/new/forum" exact component={NewForum} />
-      <Route path="/new/scp" exact component={NewScp} />
-      {/* Auth pages */}
-      <Route path="/login" exact component={Login} />
-      <Route path="/signup" exact component={Signup} />
-      {/* Single pages */}
-      <Route path="/scp/:id" exact component={Scpage} />
-      <Route path="/forum/:id" exact component={ForumPage} />
+      <Container>
+        <Route path="/" exact component={Home} />
+        {/* Post pages */}
+        <Route path="/new/forum" exact component={NewForum} />
+        <Route path="/new/scp" exact component={NewScp} />
+        {/* Auth pages */}
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+        {/* Single pages */}
+        <Route path="/scp/:id" exact component={Scpage} />
+        <Route path="/forum/:id" exact component={ForumPage} />
+      </Container>
     </Router>
   );
 }
