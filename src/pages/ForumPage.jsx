2a866dcdf817ac831b0040ad7b7c0f202e848807
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-03-15 15:27:03 +0100

    added user auth

--- src/pages/ForumPage.jsx
+++ src/pages/ForumPage.jsx
@@ -1,11 +1,27 @@
 import React, { PureComponent } from 'react'
+import axios from 'axios';
 
 class ForumPage extends PureComponent {
     constructor(props) {
         super(props)
         this.state = {
-
+            user: {}
         }
+    }
+
+    componentDidMount() {
+        const accessToken = localStorage.getItem("accessToken")
+        const refreshToken = localStorage.getItem("refreshToken")
+        const config = {
+            headers: {
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+            }
+        }
+        axios.get("http://localhost:3005/user/me", config)
+            .then((res) => {
+                this.setState({ user: res.data })
+            })
     }
 
     render() {
